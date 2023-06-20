22ebfddbadc7301acc3a675cde4186faca85c611
SowatRafi <sowad.rafi@icloud.com>
2023-06-20 15:32:36 +0600

    Configed the OnboardingView file.

--- RESTART/RESTART/Screens/OnboardingView.swift
+++ RESTART/RESTART/Screens/OnboardingView.swift
@@ -8,11 +8,25 @@
 import SwiftUI
 
 struct OnboardingView: View {
+    // SOWAD: - PROPERTY
+    @AppStorage("onboarding") var isOnboardingViewActive: Bool = true
+    // SOWAD: - BODY
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        // VStack
+        VStack(spacing: 20) {
+            Text("Onboarding").font(.largeTitle)
+            // Button
+            Button(action: {
+                isOnboardingViewActive = false
+            }) //: Button
+            { // Text
+                Text("Start")
+            } //: Text
+            
+        } //: VStack
     }
 }
-
+// SOWAD: - PREVIEW
 struct OnboardingView_Previews: PreviewProvider {
     static var previews: some View {
         OnboardingView()
