35ef01a8747f2ccdbd7ef6aaab31690a0a86a4a7
SowatRafi <sowad.rafi@icloud.com>
2023-06-20 12:46:07 +0600

    AppIcon added

---
+++ RESTART/RESTART/RESTARTApp.swift
@@ -0,0 +1,17 @@
+//
+//  RESTARTApp.swift
+//  RESTART
+//
+//  Created by Sowad Hossain Rafi on 20/6/23.
+//
+
+import SwiftUI
+
+@main
+struct RESTARTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
