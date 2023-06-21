6adf3e2c13367e979ebc07a230828a7d6e6297a7
SowatRafi <sowad.rafi@icloud.com>
2023-06-21 17:51:01 +0600

    New changed added for Home (animation, text, picture)

--- RESTART/RESTART/Screens/HomeView.swift
+++ RESTART/RESTART/Screens/HomeView.swift
@@ -1,9 +1,7 @@
-//
 //  HomeView.swift
 //  RESTART
-//
+
 //  Created by Sowad Hossain Rafi on 20/6/23.
-//
 
 import SwiftUI
 
@@ -20,11 +18,11 @@ struct HomeView: View {
             
             ZStack {
                 CircleGroupView(ShapeColor: .gray, ShapeOpacity: 0.1)
-                Image("character-2")
+                Image("character-3")
                     .resizable()
                     .scaledToFit()
                 .padding()
-                .offset(y: isAnimating ? 35 : -35)
+                .offset(y: isAnimating ? 20 : -10)
                 .animation(
                     Animation
                         .easeInOut(duration: 4)
@@ -33,17 +31,28 @@ struct HomeView: View {
                 )
             }
             // RESTART - CENTER
-            Text("The time that leads to mastery is dependent on the intensity of our focus.")
+            // Text 01
+            Text("Be patient and kind to others.")
+                .font(.system(size: 30))
+                .fontWeight(.bold)
+                .foregroundColor(.secondary)
+                .opacity(0.7)
+                .multilineTextAlignment(.center)
+                .padding()
+            // Text 02
+            Text("Eid Ul Adzha is a time to be patient and kind to others. Be understanding of those who may be struggling, and help out where you can.")
                 .font(.title3)
                 .fontWeight(.light)
                 .foregroundColor(.secondary)
                 .multilineTextAlignment(.center)
-                .padding()
+                .padding(.all)
             // RESTART - FOOTER
             Spacer()
                 // Button
                 Button(action: {
-                    isOnboardingViewActive = true
+                    withAnimation{
+                        isOnboardingViewActive = true
+                    }
                 }) //: Button
                 {
                     // Image
@@ -57,8 +66,16 @@ struct HomeView: View {
                 .buttonStyle(.borderedProminent)
                 .buttonBorderShape(.capsule)
                 .controlSize(.large)
-                
             } //:VStack
+        .opacity(isAnimating ? 1 : 0)
+        .offset(y: isAnimating ? 0 : -40)
+        .animation(.easeOut(duration: 1), value: isAnimating)
+        .onAppear(perform: {
+            // DispatchQueue: An object that manages the execution of tasks serially or concurrently on app's main thread or on background thread.
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5, execute: {
+                isAnimating = true
+            })
+        })
         } //: BODY
     }
 // SOWAD: - PREVIEW
