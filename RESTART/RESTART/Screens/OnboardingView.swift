a965d0c28aa108db6bd0a9f021e6d086e93bcabf
SowatRafi <sowad.rafi@icloud.com>
2023-06-21 15:34:44 +0600

    Button's deformable shape added.

--- RESTART/RESTART/Screens/OnboardingView.swift
+++ RESTART/RESTART/Screens/OnboardingView.swift
@@ -62,7 +62,8 @@ It's not how much we give but how much love we put into giving.
                     HStack {
                         Capsule()
                             .fill(Color("ColorRed"))
-                            .frame(width: 80)
+                            .frame(width: buttonOffset + 80)
+                        
                         Spacer()
                     }
                     // 4. CIRCLE (DRAGGABLE)
